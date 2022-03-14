f9b127578c738fadf42ab030bf5197e39ba9f4f5
Vamshidhar-reddy <vamshi777reddy@gmail.com>
2022-03-13 21:50:54 -0400

    A cricket website

---
+++ CricketWeb/js/teamRanking.js
@@ -0,0 +1,39 @@
+﻿
+function hideAndShowRanking(clickedId) {
+    var id = '';
+    if (clickedId == "testClickDiv")
+    {
+        id = "testdiv";
+        var x = document.getElementById(id);
+        document.getElementById("testId").classList.add("td-active");
+        x.style.display = "block";
+        document.getElementById("odidiv").style.display = "none";
+        document.getElementById("odiId").classList.remove("td-active");
+        document.getElementById("t20div").style.display = "none";
+        document.getElementById("t20Id").classList.remove("td-active");
+    }
+    else if (clickedId == "odiClickDiv") {
+        id = "odidiv";
+        var x = document.getElementById(id);
+
+        x.style.display = "block";
+        document.getElementById("odiId").classList.add("td-active");
+
+        document.getElementById("testdiv").style.display = "none";
+        document.getElementById("testId").classList.remove("td-active");
+        document.getElementById("t20div").style.display = "none";
+        document.getElementById("t20Id").classList.remove("td-active");
+    }
+    else if (clickedId == "t20ClickDiv") {
+        id = "t20div";
+        var x = document.getElementById(id);
+
+        x.style.display = "block";
+        document.getElementById("t20Id").classList.add("td-active");
+        document.getElementById("odidiv").style.display = "none";
+        document.getElementById("odiId").classList.remove("td-active");
+        document.getElementById("testdiv").style.display = "none";
+        document.getElementById("testId").classList.remove("td-active");
+    }
+    
+}
